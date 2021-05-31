d3eda846e2b25a969330ecb67daac3c4fa30adb1
Travis Evans <travisgevans@gmail.com>
2021-05-30 21:09:47 -0500

    Implement filtering for rapid transients such as when shaking the HAT

--- tilt_sensor.py
+++ tilt_sensor.py
@@ -8,20 +8,23 @@ from sense_hat import SenseHat
 
 class TiltSensor:
 
-    def __init__(self, hat, sensitivity=0.3, hysteresis=0.15):
+    def __init__(self, hat, sensitivity=0.3, hysteresis=0.15, debounce_delay=15):
         self._hat = hat
         self.sensitivity = sensitivity
         self.hysteresis = hysteresis
+        self.debounce_delay = debounce_delay
 
     def sense_loop(self):
         directions = {
             'x': {'low': 'left', 'high': 'right'},
             'y': {'low': 'up', 'high': 'down'},
         }
-        last_tilt = {'x': None, 'y': None}
+        last_stable_tilt = {'x': None, 'y': None}
+        last_tilt = last_stable_tilt.copy()
+        this_tilt = last_stable_tilt.copy()
+        stable_count = {'x': 0, 'y': 0}
 
         while True:
-            this_tilt = last_tilt.copy()
             accel = self._hat.get_accelerometer_raw()
 
             for axis in ('x', 'y'):
@@ -32,13 +35,18 @@ class TiltSensor:
                 elif abs(accel[axis]) < self.sensitivity - self.hysteresis:
                     this_tilt[axis] = None
 
-                if last_tilt[axis] != this_tilt[axis]:
-                    if last_tilt[axis]:
-                        print('{} released'.format(last_tilt[axis]))
+                if this_tilt[axis] == last_tilt[axis]:
+                    stable_count[axis] += 1
+                else:
+                    stable_count[axis] = 0
+                    last_tilt[axis] = this_tilt[axis]
+
+                if stable_count[axis] == self.debounce_delay:
+                    if last_stable_tilt[axis]:
+                        print('{} released'.format(last_stable_tilt[axis]))
                     if this_tilt[axis]:
                         print('{} pressed'.format(this_tilt[axis]))
-
-            last_tilt = this_tilt
+                    last_stable_tilt[axis] = this_tilt[axis]
 
 
 if __name__ == '__main__':
